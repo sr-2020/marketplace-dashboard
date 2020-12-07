7e383740dc2439b50a6e8011297afe0f19594dd3
Arival69 <ynik5643195@gmail.com>
2020-12-08 00:46:15 +0300

    создана первоначальная верстка

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Header from './common/header/Header'
+import Nav from './common/nav/Nav'
+import Footer from './common/footer/Footer'
+import Main from './common/main/Main'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Nav></Nav>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/common/nav/Nav.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Nav.css'
+
+function Nav() {
+  return (
+    <nav className='nav'>
+      Admin Template
+      <a href='#' className='nav-link'>
+        123456789
+      </a>
+      <a href='#' className='nav-link'>
+        123456789
+      </a>
+      <a href='#' className='nav-link'>
+        123456789
+      </a>
+      <a href='#' className='nav-link'>
+        123456789
+      </a>
+    </nav>
+  )
+}
+
+export default Nav
