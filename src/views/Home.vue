5f4ba2258f4232e5df825c4f4d91c5e9b32e08b5
Dmitriy Dzyuba <tzar4eg@gmail.com>
2021-02-05 19:45:06 +0300

    added grid

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,17 @@
 <template>
-  <div class="home">
-    HelloWorld
+  <div class="container home">
+    <div class="row">
+      <div class="col-m-3">
+        HelloWorld
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from "vue-property-decorator";
+import { Component, Vue } from 'vue-property-decorator'
 
-@Component({ })
-export default class Home extends Vue {}
+@Component({})
+export default class Home extends Vue {
+}
 </script>
