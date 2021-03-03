c35a8459cac4ce56a31e83d30fe03d56dcabd07e
Dmitriy Dzyuba <tzar4eg@gmail.com>
2021-03-03 08:52:57 +0300

    lint

--- src/components/Corporation/CorporationListItem.vue
+++ src/components/Corporation/CorporationListItem.vue
@@ -1,34 +1,27 @@
 <template>
   <div class="item-wrapper">
     <div class="item">
-      <div>  {{ i.name }}</div>
-      <div v-if="i.owner">
-        Владелец: {{ i.owner.name }}
-      </div>
+      <div>{{ i.name }}</div>
+      <div v-if="i.owner">Владелец: {{ i.owner.name }}</div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator'
-import { Corporation } from '@/store/organisations/types'
-
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { Corporation } from "@/store/organisations/types";
 
 @Component({})
 export default class CorporationListItem extends Vue {
-  @Prop() i!: Corporation
-
+  @Prop() i!: Corporation;
 }
-
 </script>
 
-<style scoped
-       lang="less">
+<style scoped lang="less">
 .item {
   padding: 12px 4px;
   display: flex;
   justify-content: space-between;
   flex-wrap: wrap;
-
 }
 </style>
