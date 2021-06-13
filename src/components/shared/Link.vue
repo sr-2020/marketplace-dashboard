737d0643fd24ac67b82e67fdc6f613f2c1a91ec6
Dmitriy Dzyuba <tzar4eg@gmail.com>
2021-06-13 12:57:39 +0300

    added link component

---
+++ src/components/shared/Link.vue
@@ -0,0 +1,30 @@
+<template>
+  <span class="entity-link" @click="onClick">
+    <slot />
+  </span>
+</template>
+
+<script lang="ts">
+import { Prop, Vue } from 'vue-property-decorator'
+
+export type SrEntity = 'user' | 'corporation'
+
+export default class SrEntityLink extends Vue {
+  @Prop({ default: '' }) type!: SrEntity
+  @Prop({ default: 0 }) entityId!: number
+
+  onClick() {
+    this.$router.push(`/${this.type}s/${this.entityId}`)
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.entity-link {
+  cursor: pointer;
+  text-decoration: underline;
+  &:hover {
+    color: var(--accent);
+  }
+}
+</style>
