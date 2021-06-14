a38e398a90e6a4c64c1f9ea748ae5590cbfae1b0
Arival69 <ynik5643195@gmail.com>
2021-06-14 17:33:52 +0300

    add-users

---
+++ src/views/Users/UsersList.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import List from '@/components/common/list/List.vue'
+import { User } from '@/store/user/types'
+import UsersListItem from './UsersListItem.vue'
+import { Options } from 'vue-class-component'
+
+@Options({})
+export default class UsersList extends List<User> {
+  listItem = UsersListItem
+  listName = 'Пользователи'
+  key = 'users'
+}
+</script>
